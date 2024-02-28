3f92b098ee3d3bea13361949365b05530f5711f2
akibaz <bavakumovic@gmail.com>
2024-02-28 15:02:14 +0100

    Create new DAO implementation for JDBCTemplate
    Create class CustomerJDBCDataAccessService which implements CustomerDAO
    interface. Work the implemented methods later. Annotate the class with
    proper repositry annotation and give it a name of jdbc. Create field
    jdbcTemplate and make Spring inject it in costructor from AplicationContext.

---
+++ springBootApp/src/main/java/com/akibazcode/customer/CustomerJDBCDataAccessService.java
@@ -0,0 +1,51 @@
+package com.akibazcode.customer;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository("jdbc")
+public class CustomerJDBCDataAccessService implements CustomerDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    public CustomerJDBCDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public List<Customer> selectAllCustomers() {
+        return null;
+    }
+
+    @Override
+    public Optional<Customer> selectCustomerById(Integer customerId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void insertCustomer(Customer customer) {
+
+    }
+
+    @Override
+    public boolean existsCustomerWithEmail(String email) {
+        return false;
+    }
+
+    @Override
+    public boolean existsCustomerWithId(Integer customerId) {
+        return false;
+    }
+
+    @Override
+    public void deleteCustomerById(Integer customerId) {
+
+    }
+
+    @Override
+    public void updateCustomer(Customer update) {
+
+    }
+}
