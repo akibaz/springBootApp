e457e91eebb8a996a10d368efdc2776bf09b5c3c
akibaz <bavakumovic@gmail.com>
2024-04-03 08:29:40 +0200

    Configure app for unit testing of data access layer
    Add required dependencies for testcontainers, junit and postgres to pom.
    Create test class to verify that everything works. Test whether postgres
    is created and flyway migrations. Link our app to testcontainers by
    specifiying datasource properties in @DynamicPropertySource.

---
+++ springBootApp/src/test/java/com/akibazcode/TestcontainersTest.java
@@ -0,0 +1,54 @@
+package com.akibazcode;
+
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+public class TestcontainersTest {
+
+    @Container
+    private static final PostgreSQLContainer<?> postgreSQLContainer =
+            new PostgreSQLContainer<>("postgres:latest")
+                    .withDatabaseName("akibazcode-dao-unit-test")
+                    .withUsername("akibaz")
+                    .withPassword("password");
+
+    @DynamicPropertySource
+    private static void registerDataSourceProperties(DynamicPropertyRegistry registry) {
+        registry.add(
+                "spring.datasource.url",
+                () -> postgreSQLContainer.getJdbcUrl()
+                );
+        registry.add(
+                "spring.datasource.username",
+                () -> postgreSQLContainer.getUsername()
+        );
+        registry.add(
+                "spring.datasource.password",
+                postgreSQLContainer::getPassword
+        );
+    }
+
+    @Test
+    void canStartPostgresDB() {
+        assertThat(postgreSQLContainer.isCreated()).isTrue();
+        assertThat(postgreSQLContainer.isRunning()).isTrue();
+    }
+
+    @Test
+    void canApplyDbMigrationsWithFlyway() {
+        Flyway flyway = Flyway.configure().dataSource(
+                postgreSQLContainer.getJdbcUrl(),
+                postgreSQLContainer.getUsername(),
+                postgreSQLContainer.getPassword()
+        ).load();
+        flyway.migrate();
+    }
+}
