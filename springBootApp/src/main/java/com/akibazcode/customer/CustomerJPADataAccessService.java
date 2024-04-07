707513b548547944b57d3e51e05ca9de18243d2b
akibaz <bavakumovic@gmail.com>
2024-04-07 10:20:48 +0200

    Create test for jpa data access
    Create test for CustomerRepository. Use proper annotation instead of
    manually creating required beans. There are no custom methods defined in
    CustomerJPADataAccessService so we wont test it. We will test only
    user defined methods in CustomerRepository.

--- springBootApp/src/main/java/com/akibazcode/customer/CustomerJPADataAccessService.java
+++ springBootApp/src/main/java/com/akibazcode/customer/CustomerJPADataAccessService.java
@@ -1,6 +1,5 @@
 package com.akibazcode.customer;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
--- springBootApp/src/main/java/com/akibazcode/customer/CustomerRepository.java
+++ springBootApp/src/main/java/com/akibazcode/customer/CustomerRepository.java
@@ -3,6 +3,7 @@ package com.akibazcode.customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
     boolean existsCustomerByEmail(String email);
 
     boolean existsCustomerById(Integer customerId);
