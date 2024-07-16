75d749ef8731172e5abc822ef34648249f5495b1
akibaz <bavakumovic@gmail.com>
2024-07-16 13:17:00 +0200

    Remove unused imports and add some spacing

--- backend/src/main/java/com/akibazcode/Main.java
+++ backend/src/main/java/com/akibazcode/Main.java
@@ -4,19 +4,18 @@ import com.akibazcode.customer.Customer;
 import com.akibazcode.customer.CustomerRepository;
 import com.github.javafaker.Faker;
 import com.github.javafaker.Name;
-import org.springframework.boot.Banner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.List;
 import java.util.Random;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+
     }
 
     @Bean
